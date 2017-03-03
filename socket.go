6698c7da190b4c5342c4146dd0beddf3e79dacd9
pengwei <pengwei@teambition.com>
2017-03-03 10:06:22 +0800

    Add ReadAll func

--- socket.go
+++ socket.go
@@ -1,6 +1,8 @@
 package socket
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"net"
 	"sync"
@@ -35,6 +37,28 @@ func (s *Socket) Use() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.isUse = true
+}
+
+// ReadAll ...
+func (s *Socket) ReadAll(initialCap ...int) (datas []byte, err error) {
+	initial := 4096
+	if len(initialCap) > 0 && initialCap[0] > 0 {
+		initial = initialCap[0]
+	}
+	request := make([]byte, initial)
+	var jsonBuf bytes.Buffer
+	var n int
+	for {
+		n, err = s.Read(request)
+		fmt.Print(n)
+		if n > 0 {
+			jsonBuf.Write(request[0:n])
+		}
+		if n < initial || err != nil {
+			break
+		}
+	}
+	return jsonBuf.Bytes(), err
 }
 
 // Ping to detect whether the socket is closed.
